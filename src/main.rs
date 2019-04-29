9ed311e490bb09937c6f4c442be4da2b69c351b5
Blaine Price <1wbprice@gmail.com>
2019-04-29 12:53:12 -0400

    Add examples for showing items of various types.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use termion::raw::IntoRawMode;
+use std::io::{Write, stdout};
 use termion::{color, clear, style};
 use chrono::{Utc};
 
@@ -12,4 +14,14 @@ fn main() {
             bold = style::Bold,
             date = Utc::now().format("%a, %b %e").to_string(),
             reset = color::Fg(color::Reset));
+
+    println!("\u{2022} a task!");
+    println!("\u{0058} a completed task!");
+    println!("\u{26AC} an event!");
+    println!("\u{2013} a note!");
+
+    let mut stdout = stdout().into_raw_mode().unwrap();
+    writeln!(stdout, "Hey there.").unwrap();
+
+
 }
