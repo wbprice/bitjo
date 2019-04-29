3aab74a7aae4ee0708f2f192aee40db5b9dfa085
Blaine Price <1wbprice@gmail.com>
2019-04-29 12:34:34 -0400

    Add chrono dependency.  Print current date on startup screen.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
-use termion::{color, clear};
+use termion::{color, clear, style};
+use chrono::{Utc};
+
 
 fn main() {
     println!("{}", clear::All);
     println!("{green}Bit Journal v0.1.0{reset}",
            green = color::Fg(color::Green),
            reset = color::Fg(color::Reset));
+    println!("{yellow}Today is {bold}{date}.{reset}",
+            yellow = color::Fg(color::Yellow),
+            bold = style::Bold,
+            date = Utc::now().format("%a, %b %e").to_string(),
+            reset = color::Fg(color::Reset));
 }
