b5c59368d589fb5ef34d2e7958604dd32c50f955
Blaine Price <1wbprice@gmail.com>
2019-04-29 11:05:51 -0400

    Update name

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use termion::{color, clear};
 
 fn main() {
     println!("{}", clear::All);
-    println!("{green}Bullet Journal CLI v0.1.0{reset}",
+    println!("{green}Bit Journal v0.1.0{reset}",
            green = color::Fg(color::Green),
            reset = color::Fg(color::Reset));
 }
