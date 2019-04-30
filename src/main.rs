872de46c953e6542fa26ac7000dc67d6fccf3509
Blaine Price <1wbprice@gmail.com>
2019-04-30 00:33:40 -0400

    Create structs for each type of journal item

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,85 @@ use std::io::{stdout, Write};
 use termion::raw::IntoRawMode;
 use termion::{clear, color, style};
 
+#[derive(Default)]
+struct Event {
+    important: bool,
+    content: String,
+}
+
+#[derive(Default)]
+struct Task {
+    important: bool,
+    completed: bool,
+    cancelled: bool,
+    content: String,
+}
+
+#[derive(Default)]
+struct Note {
+    important: bool,
+    content: String,
+}
+
+trait JournalItem<T> {
+    fn print(&self);
+    fn new(content: String) -> T;
+}
+
+impl JournalItem<Event> for Event {
+    fn print(&self) {
+        println!(
+            "{important} {symbol} {content}",
+            important = if self.important { "*" } else { " " },
+            symbol = "\u{26AC}",
+            content = self.content
+        )
+    }
+
+    fn new(content: String) -> Event {
+        Event {
+            content: content,
+            ..Default::default()
+        }
+    }
+}
+
+impl JournalItem<Task> for Task {
+    fn print(&self) {
+        println!(
+            "{important} {symbol} {content}",
+            important = if self.important { "*" } else { " " },
+            symbol = if self.completed { "X" } else { "\u{2022}" },
+            content = self.content
+        )
+    }
+
+    fn new(content: String) -> Task {
+        Task {
+            content: content,
+            ..Default::default()
+        }
+    }
+}
+
+impl JournalItem<Note> for Note {
+    fn print(&self) {
+        println!(
+            "{important} {symbol} {content}",
+            important = if self.important { "*" } else { " " },
+            symbol = "\u{2013}",
+            content = self.content
+        )
+    }
+
+    fn new(content: String) -> Note {
+        Note {
+            content: content,
+            ..Default::default()
+        }
+    }
+}
+
 fn main() {
     println!("{}", clear::All);
     println!(
@@ -18,11 +97,14 @@ fn main() {
         reset = color::Fg(color::Reset)
     );
 
-    println!("\u{2022} a task!");
-    println!("\u{0058} a completed task!");
-    println!("\u{26AC} an event!");
-    println!("\u{2013} a note!");
+    let task = Task::new("A task!".to_string());
+    let mut completed_task = Task::new("A completed task!".to_string());
+    completed_task.completed = true;
+    let event = Event::new("An event!".to_string());
+    let note = Note::new("A note!".to_string());
 
-    let mut stdout = stdout().into_raw_mode().unwrap();
-    writeln!(stdout, "Hey there.").unwrap();
+    task.print();
+    completed_task.print();
+    event.print();
+    note.print();
 }
