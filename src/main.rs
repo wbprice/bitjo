4e8a8d017835f04ccabdf2e1256063a3ee53cfa1
Blaine Price <1wbprice@gmail.com>
2019-05-01 18:19:48 -0400

    Create vector of structs with different properties with common methods

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 use chrono::Local;
 use termion::{clear, color, style};
 
-#[derive(Default, Clone)]
+#[derive(Debug, Default, Clone)]
 struct Event {
     important: bool,
     content: String,
 }
 
-#[derive(Default, Clone)]
+#[derive(Debug, Default, Clone)]
 struct Task {
     important: bool,
     completed: bool,
@@ -15,139 +15,72 @@ struct Task {
     content: String,
 }
 
-#[derive(Default, Clone)]
+#[derive(Debug, Default, Clone)]
 struct Note {
     important: bool,
     content: String,
 }
 
-trait JournalItem<T> {
-    fn render(&self);
-    fn new(content: String) -> T;
-    fn toggle_important(&self) -> T;
-    fn set_content(&self, content: String) -> T;
-}
-
 #[derive(Debug)]
-enum JournalItems {
-    Event,
-    Task,
-    Note
+enum Entries {
+    Event(Event),
+    Task(Task),
+    Note(Note)
 }
 
-trait Completable<T> {
-    fn toggle_completed(&self) -> T;
+trait Journalable {
+    fn render(&self) -> String;
+    fn toggle_completed(&self) -> Entries;
+    fn set_content(&self, content: String) -> Entries;
 }
 
-trait Cancellable<T> {
-    fn toggle_cancellation(&self) -> T;
-}
-
-impl JournalItem<T> for JournalItems {
-
-}
-
-impl JournalItem<Event> for Event {
-    fn render(&self) {
-        println!(
-            "{important} {symbol} {content}",
-            important = if self.important { "*" } else { " " },
-            symbol = "\u{26AC}",
-            content = self.content
-        )
-    }
-
-    fn new(content: String) -> Event {
-        Event {
-            content: content,
-            ..Default::default()
+impl Journalable for Entries {
+    fn render(&self) -> String {
+        match self {
+            Entries::Event(item) => format!("{important} {symbol} {content}",
+                important = if item.important { "*" } else {" "},
+                symbol = "\u{26AC}",
+                content = item.content),
+            Entries::Task(item) => format!("{important} {symbol} {content}",
+                important = if item.important { "*" } else {" "},
+                symbol = if item.completed { "X" } else { "\u{2022}" },
+                content = item.content),
+            Entries::Note(item) => format!("{important} {symbol} {content}",
+                important = if item.important { "*" } else {" "},
+                symbol = "-",
+                content = item.content),
         }
     }
 
-    fn toggle_important(&self) -> Event {
-        Event {
-            important: !self.important,
-            ..self.clone()
+    fn toggle_completed(&self) -> Entries {
+        match self {
+            Entries::Task(item) => Entries::Task(Task {
+                completed: !item.completed,
+                ..item.clone()
+            }),
+            Entries::Note(note) => Entries::Note(Note {
+                ..note.clone()
+            }),
+            Entries::Event(event) => Entries::Event(Event {
+                ..event.clone()
+            })
         }
     }
 
-    fn set_content(&self, content: String) -> Event {
-        Event {
-            content: content.into(),
-            ..self.clone()
-        }
-    }
-}
-
-impl Completable<Task> for Task {
-    fn toggle_completed(&self) -> Task {
-        Task {
-            completed: !self.completed,
-            ..self.clone()
-        }
-    }
-}
-
-impl JournalItem<Task> for Task {
-    fn render(&self) {
-        println!(
-            "{important} {symbol} {content}",
-            important = if self.important { "*" } else { " " },
-            symbol = if self.completed { "X" } else { "\u{2022}" },
-            content = self.content
-        )
-    }
-
-    fn new(content: String) -> Task {
-        Task {
-            content: content,
-            ..Default::default()
-        }
-    }
-
-    fn toggle_important(&self) -> Task {
-        Task {
-            important: !self.important,
-            ..self.clone()
-        }
-    }
-
-    fn set_content(&self, content: String) -> Task {
-        Task {
-            content: content.into(),
-            ..self.clone()
-        }
-    }
-}
-
-impl JournalItem<Note> for Note {
-    fn render(&self) {
-        println!(
-            "{important} {symbol} {content}",
-            important = if self.important { "*" } else { " " },
-            symbol = "\u{2013}",
-            content = self.content
-        )
-    }
-
-    fn new(content: String) -> Note {
-        Note {
-            content: content,
-            ..Default::default()
-        }
-    }
-
-    fn toggle_important(&self) -> Note {
-        Note {
-            important: !self.important,
-            ..self.clone()
-        }
-    }
-
-    fn set_content(&self, content: String) -> Note {
-        Note {
-            content: content.into(),
-            ..self.clone()
+    fn set_content(&self, content: String) -> Entries {
+        match self {
+            Entries::Task(item) => Entries::Task(Task {
+                content: content.into(),
+                ..item.clone()
+            }),
+            Entries::Note(note) => Entries::Note(Note {
+                content: content.into(),
+                ..note.clone()
+            }),
+            Entries::Event(event) => Entries::Event(Event {
+                content: content.into(),
+                ..event.clone()
+            })
         }
     }
 }
@@ -167,28 +100,28 @@ fn main() {
         reset = color::Fg(color::Reset)
     );
 
-    let task = Task::new("A task!".to_string());
-    let mut completed_task = Task::new("A completed task!".to_string());
-    completed_task.completed = true;
-    let important_task = completed_task
-        .toggle_important()
-        .set_content("An important, completed task".into());
-    let event = Event::new("An event!".to_string());
-    let note = Note::new("A note!".to_string());
-
-    task.render();
-    completed_task.render();
-    important_task.render();
-    event.render();
-    note.render();
-
-    let tasklist : Vec<JournalItems> = vec![
-        JournalItems::Task::new("A task!".into()),
-        JournalItems::Task::new("A completed task!".into()).toggle_completed(),
-        JournalItems::Task::new("An important, completed task!".into()).toggle_completed().toggle_important(),
-        JournalItems::Event::new("An event!".into()),
-        JournalItems::Note::new("A note!".into())
+    let memory : Vec<Entries> = vec![
+        Entries::Event(Event {
+            content: "Internal Standup at 4pm".into(),
+            ..Default::default()
+        }),
+        Entries::Task(Task {
+            content: "Figure out enums".into(),
+            ..Default::default()
+        }),
+        Entries::Task(Task {
+            content: "Take out the trash".into(),
+            ..Default::default()
+        })
+        .toggle_completed()
+        .set_content("Laugh uncontrollably".into()),
+        Entries::Note(Note {
+            content: "I'm just surprised this worked".into(),
+            ..Default::default()
+        }),
     ];
 
-    dbg!(tasklist);
+    for entry in memory {
+        println!("{}", entry.render());
+    }
 }
