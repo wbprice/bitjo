679709cce3c058b9b0cd382913b7a07c2dae4bf9
Blaine Price <blaine@ario.com>
2021-02-18 09:54:15 -0500

    stuff

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,48 @@
-use structopt::StructOpt;
+extern crate termion;
 
-mod controllers;
-mod models;
-mod services;
-mod views;
-
-use crate::{
-    controllers::{Command, EntryOpts, Opt},
-    models::{Entry, EntryVariants},
-    services::{Journalable, LocalDiskJournal},
-    views::Application,
-};
+use termion::event::Key;
+use termion::input::TermRead;
+use termion::raw::IntoRawMode;
+use std::io::{Write, stdout, stdin};
 
 fn main() {
-    let opt = Opt::from_args();
+    // Get the standard input stream.
+    let stdin = stdin();
+    // Get the standard output stream and go to raw mode.
+    let mut stdout = stdout().into_raw_mode().unwrap();
 
-    let mut journal = LocalDiskJournal::new(None);
+    write!(stdout, "{}{}q to exit. Type stuff, use alt, and so on.{}",
+           // Clear the screen.
+           termion::clear::All,
+           // Goto (1,1).
+           termion::cursor::Goto(1, 1),
+           // Hide the cursor.
+           termion::cursor::Hide).unwrap();
+    // Flush stdout (i.e. make the output appear).
+    stdout.flush().unwrap();
 
-    // Handle input!
-    if let Some(command) = opt.command {
-        match command {
-            Command::Add { new_entry } => match new_entry {
-                EntryOpts::Event { text } => journal.append(Entry::new(EntryVariants::Event, text)),
-                EntryOpts::Note { text } => journal.append(Entry::new(EntryVariants::Note, text)),
-                EntryOpts::Task { text } => journal.append(Entry::new(EntryVariants::Task, text)),
-            },
-            Command::Emph { index } => {
-                journal.toggle_importance(index);
-            }
-            Command::Complete { index } => {
-                journal.toggle_completion(index);
-            }
-            Command::Remove { index } => journal.remove(index),
+    for c in stdin.keys() {
+        // Clear the current line.
+        write!(stdout, "{}{}", termion::cursor::Goto(1, 1), termion::clear::CurrentLine).unwrap();
+
+        // Print the key we type...
+        match c.unwrap() {
+            // Exit.
+            Key::Char('q') => break,
+            Key::Char(c)   => println!("{}", c),
+            Key::Alt(c)    => println!("Alt-{}", c),
+            Key::Ctrl(c)   => println!("Ctrl-{}", c),
+            Key::Left      => println!("<left>"),
+            Key::Right     => println!("<right>"),
+            Key::Up        => println!("<up>"),
+            Key::Down      => println!("<down>"),
+            _              => println!("Other"),
         }
+
+        // Flush again.
+        stdout.flush().unwrap();
     }
 
-    let application = Application {
-        entries: journal.list(),
-    };
-
-    application.render();
+    // Show the cursor again before we exit.
+    write!(stdout, "{}", termion::cursor::Show).unwrap();
 }
